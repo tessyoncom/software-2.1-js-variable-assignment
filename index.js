e265ca1677d6bae1c2a346ad97c1348a9efaaa6b
Tesilimi Yusuf <tessyoncom@gmail.com>
2023-06-02 20:08:07 +0100

    new update

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ let passingGrade = 80;
 let average = (grade1+grade2+grade3+grade4)/4;
 studentLastName = "Newman";
 console.log(studentFirstName, studentLastName, "has an average grade of", average+".");
-if (average > passingGrade){
-    console.log(studentFirstName, StudentLastName, "passed")
-}
-else{
-    console.log(studentFirstName, studentLastName, "failed")
-}
+
+console.log(`Did ${studentFirstName} ${studentLastName} pass? ${average>passingGrade}`)
+
+//1.Create a variable called average that computes for the average from grade1 to grade4
+
+//2. Change the value of studentLastName to "Newman" 
+
+//3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
+//ex. Jane Doe has an average grade of 100.
+
+//4. Write code that would answer this question: Did the student pass?
